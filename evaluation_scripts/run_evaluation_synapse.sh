f68fe3f73b99002619c2de411521ac2506526e5a
AndreaDosiUniversity <andrea.dosi@unina.it>
2025-03-05 21:32:21 +0100

    changes for ibisco

--- evaluation_scripts/run_evaluation_synapse.sh
+++ evaluation_scripts/run_evaluation_synapse.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-DATASET_PATH=DATASET_Synapse
+#DATASET_PATH=DATASET_Synapse
 #CHECKPOINT_PATH=../unetr_pp/evaluation/unetr_pp_synapse_checkpoint
-CHECKPOINT_PATH=/home/dottorato_coding/output_synapse
+#CHECKPOINT_PATH=/home/dottorato_coding/output_synapse
+
+#export PYTHONPATH=./
+#export RESULTS_FOLDER="$CHECKPOINT_PATH"
+#export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task02_Synapse
+#export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
+
+#python unetr_pp/run/run_training.py 3d_fullres amber_trainer_synapse 2 0 -val
+
+
+DATASET_PATH=/ibiscostorage/adosi/DATASET_Synapse
+#CHECKPOINT_PATH=../unetr_pp/evaluation/unetr_pp_synapse_checkpoint
+CHECKPOINT_PATH=/ibiscostorage/adosi/output_synapse
 
 export PYTHONPATH=./
 export RESULTS_FOLDER="$CHECKPOINT_PATH"
--- training_scripts/run_training_synapse.sh
+++ training_scripts/run_training_synapse.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
-DATASET_PATH=DATASET_Synapse
+###### LOCAL ######
+
+#DATASET_PATH=DATASET_Synapse
+
+#export PYTHONPATH=./
+#export RESULTS_FOLDER=../output_synapse
+#export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task02_Synapse
+#export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
+
+#python unetr_pp/run/run_training.py 3d_fullres amber_trainer_synapse 2 0
+
+
+#### IBISCO #####
+
+DATASET_PATH=/ibiscostorage/adosi/DATASET_Synapse
 
 export PYTHONPATH=./
-export RESULTS_FOLDER=../output_synapse
+export RESULTS_FOLDER=/ibiscostorage/adosi/output_synapse
 export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task02_Synapse
 export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
 
 python unetr_pp/run/run_training.py 3d_fullres amber_trainer_synapse 2 0
+
+
+
