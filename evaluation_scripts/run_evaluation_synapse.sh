478cdc6568fe87df5d9219daa311c1bbae2718d9
AndreaDosiUniversity <andrea.dosi@unina.it>
2025-03-05 19:04:44 +0100

    first massive commit

---
+++ evaluation_scripts/run_evaluation_synapse.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DATASET_PATH=DATASET_Synapse
+#CHECKPOINT_PATH=../unetr_pp/evaluation/unetr_pp_synapse_checkpoint
+CHECKPOINT_PATH=/home/dottorato_coding/output_synapse
+
+export PYTHONPATH=./
+export RESULTS_FOLDER="$CHECKPOINT_PATH"
+export unetr_pp_preprocessed="$DATASET_PATH"/unetr_pp_raw/unetr_pp_raw_data/Task02_Synapse
+export unetr_pp_raw_data_base="$DATASET_PATH"/unetr_pp_raw
+
+python unetr_pp/run/run_training.py 3d_fullres amber_trainer_synapse 2 0 -val
